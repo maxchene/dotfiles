ada2608ce56bda344d405f9d615e8593e6e252fd
max <maxime@kaliel.fr>
2026-08-12 15:21:52 +0200

    fix: changer hypr to lua config

---
+++ hypr/monitors/home.lua
@@ -0,0 +1,20 @@
+hl.monitor({
+    output = "DP-4",
+    mode = "2560x1440@59.95",
+    position = "2560x0",
+    scale = 1,
+})
+ 
+hl.monitor({
+    output = "DP-3",
+    mode = "2560x1440@59.95",
+    position = "0x0",
+    scale = 1,
+})
+ 
+hl.monitor({
+    output = "eDP-1",
+    mode = "1920x1080@60",
+    position = "720x1440",
+    scale = 1,
+})
